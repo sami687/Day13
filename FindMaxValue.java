917ef2efdbc0476f0eddce14ec64b617b71ff742
samiksha shende <samikshashende34@gmail.com>
2021-10-17 20:02:07 +0530

    ADDED : Find max value

---
+++ FindMaxValue.java
@@ -0,0 +1,17 @@
+package com.bl.generics;
+
+public class FindMaxValue {
+
+    public static Integer maxOfInteger(Integer a1, Integer a2, Integer a3){
+        Integer max = a1;
+        if (a2.compareTo(max) > 0)
+            max = a2;
+        if (a3.compareTo(max) > 0)
+            max = a3;
+        return max;
+    }
+
+	public static void main(String[] args) {
+        System.out.println("The maximum value between the three integer is : " + maxOfInteger(3, 8, 5));
+}
+}
