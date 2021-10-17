b65d7293bc0e756eb6396990c4f14f29cd421e6a
samiksha shende <samikshashende34@gmail.com>
2021-10-17 21:07:49 +0530

    ADDED : Find Max Value

--- FindMaxValue.java
+++ FindMaxValue.java
@@ -1,16 +1,19 @@
 package com.bl.generics;
 
 public class FindMaxValue {
-	public static String maxOfString(String s1, String s2, String s3) {
-		String max = s1;
-		if (s2.compareTo(max) > 0)
-			max = s2;
-		if (s3.compareTo(max) > 0)
-			max = s3;
-		return max;
-	}
+    public static <T extends Comparable<T>> T maxOfValues(T a1, T a2, T a3) {
+        T max = a1;
+        if (a2.compareTo(max) > 0)
+            max = a2;
+        if (a3.compareTo(max) > 0)
+            max = a3;
+        return max;
+    }
 
 	public static void main(String[] args) {
-		System.out.println("The maximum value between the three string is : " + maxOfString("abc", "pqr", "xyz"));
+        System.out.println("The maximum value between the three integer is : " + maxOfValues(3, 8, 5)+ "\n");
+        System.out.println("The maximum value between the three float is : " + maxOfValues(1.5f, 3.56f, 5.87f) + "\n");
+        System.out.println("The maximum value between the three string is : " + maxOfValues("Apple", "Peach", "Banana"));
+
 	}
 }
